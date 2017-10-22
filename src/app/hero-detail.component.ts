357c9685722fb0eab124807052a6b7a46f7e86e7
Paul-Arthur Dalbis <dalbis.arthur@gmail.com>
2017-10-22 17:51:21 +0200

    - hero-detail importé et déclaré dans app.module - hero-detail.component créé - attribut hero-detail.component déclaré en tant que Input property - hero-detail template créé

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'hero-detail',
+  templateUrl:'hero-detail.component.html',
 })
 
 export class HeroDetailComponent {
-
+  @Input() hero: Hero;
 }
