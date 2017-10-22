de816dca1d0a55e2391f7f7d41eacb9ab5629257
Paul-Arthur Dalbis <dalbis.arthur@gmail.com>
2017-10-22 18:09:11 +0200

    stopped at Inject the HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
 @Injectable()
-export 
+export class HeroService {
+  getHeroes(): Hero[] { return HEROES; }
+}
+
